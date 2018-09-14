5aafd02f8f7d0f2bd3e298f2ed63efda5701b2bb
Axel Haustant <noirbizarre@gmail.com>
2018-09-14 11:52:48 +0200

    Set version to 1.2.0.dev

--- udata_ods/__init__.py
+++ udata_ods/__init__.py
@@ -4,5 +4,5 @@ OpenDataSoft integration for udata
 '''
 from __future__ import unicode_literals
 
-__version__ = '1.1.1.dev'
+__version__ = '1.2.0.dev'
 __description__ = 'OpenDataSoft integration for udata'
