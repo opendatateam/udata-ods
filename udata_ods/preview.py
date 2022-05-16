2765bfc20a021cc86a83887793773fb0a2df3be2
quaxsze <plum.stanislas@gmail.com>
2022-05-16 15:11:15 +0200

    fix weak reference

--- udata_ods/preview.py
+++ udata_ods/preview.py
@@ -1,6 +1,6 @@
 from flask import url_for
 
-from udata.core.dataset.models import Resource
+from udata.core.dataset.models import Resource, Dataset
 from udata.core.dataset.preview import PreviewPlugin
 
 
@@ -8,13 +8,13 @@ class OdsPreview(PreviewPlugin):
     def can_preview(self, resource):
         if not isinstance(resource, Resource):
             return
-        dataset = resource.dataset
+        dataset = Dataset.objects(resources__id=resource.id).first()
         if not dataset.extras.get('ods:url'):
             return
         return resource.extras.get('ods:type') == 'api'
 
     def preview_url(self, resource):
-        dataset = resource.dataset
+        dataset = Dataset.objects(resources__id=resource.id).first()
         return url_for('ods.preview', _external=True, _scheme='',
                        domain=dataset.extras['harvest:domain'],
                        id=dataset.extras['harvest:remote_id'])
