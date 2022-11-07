fb7bc978561fcf4162d8734c3795acc2a536bef3
maudetes <maudet.estelle@gmail.com>
2022-11-07 16:43:56 +0100

    Improve harvest fields description

--- udata_ods/models.py
+++ udata_ods/models.py
@@ -6,9 +6,10 @@ dataset_harvest_fields['ods_url'] = fields.String(
 dataset_harvest_fields['ods_references'] = fields.String(
     description='The ods reference for ods  harvested dataset', allow_null=True)
 dataset_harvest_fields['ods_has_records'] = fields.Boolean(
-    description='boolean for ods records', allow_null=True)  # TODO: what description?
+    description='boolean for ods has_records', allow_null=True)
 dataset_harvest_fields['ods_geo'] = fields.Boolean(
-    description='boolean for ods geo', allow_null=True)  # TODO: what description?
+    description='boolean for geo in dataset features', allow_null=True)
 
 resource_harvest_fields['ods_type'] = fields.String(
-    description='The ods type for ods harvest dataset', allow_null=True)  # TODO: what description?
+    description='The ods type for ods harvest dataset: api, attachment or alternative_export',
+    allow_null=True)
